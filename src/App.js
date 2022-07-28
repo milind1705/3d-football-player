e2ef7737a1b62b65532d127a43d75ac47ea5f1c0
rakshitak <rakshita.kotian@vistaura.com>
2022-07-28 11:45:37 +0530

    shadow updated

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ import Ground from './component/Ground';
 import Player from './component/Player'
 import Sun from './component/Sun'
 import Ball from './component/Ball';
-import Footballplayerblue from './component/Footballplayerblue';
+// import Footballplayerblue from './component/Footballplayerblue';
 import Football from './component/Football'
 import Dragable from './component/Dragable';
+// import Lights from './component/lights';
+import Floor from './component/Floor';
 
-
+function Plane({ ...props }) {
+  return (
+    <mesh {...props} receiveShadow>
+      <planeBufferGeometry args={[500, 500, 1, 1]} />
+      <shadowMaterial transparent opacity={0.2} />
+    </mesh>
+  )
+} 
 const PlayingBall = ()=>{
   return(
     <>
@@ -34,11 +43,32 @@ const PlayingBall = ()=>{
   )
 }
 function App() {
- 
+ const d = 8.25
   return (
     <div style={{height:'100vh', width: '100vw', }}>
-      <Canvas style={{background:'black'}} camera={{position:[7,7,7]}}>
+      <Canvas shadows dpr={[1, 1.5]} camera={{ position: [-5, 4, 4] }}>
+      <directionalLight
+        position={[10,10,10]}
+        shadow-camera-left={d * -1}
+        shadow-camera-bottom={d * -1}
+        shadow-camera-right={d}
+        shadow-camera-top={d}
+        shadow-camera-near={0.1}
+        shadow-camera-far={1500}
+        castShadow
+      />
+       {/* <Plane rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -10, 0]}color={'Green'}/> */}
+        {/* <Lights/> */}
         <Orbit />
+        
+        {/* <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <shadowMaterial 
+            attach='material' opacity={0.3} />
+          </mesh> */}
         <Sun position={[0, 6, 6]}/>
         {/* <Sun position={[0, 6, -5]}/> */}
       <ambientLight intensity={0.5}/>
@@ -53,6 +83,7 @@ function App() {
         </Suspense>
         <Ground />
           </Physics>
+          <Floor />
       </Canvas>
     </div>
   );
--- src/component/Ground.jsx
+++ src/component/Ground.jsx
@@ -1,14 +1,11 @@
-
 import { useBox } from "@react-three/cannon"
-
 const Ground = props=>{
   const [ref, api] = useBox(()=>({args:[10,0,10],...props}))
   return(
-    <mesh ref={ref} api={api}{...props} receiveShadow>
+    <mesh castshadow ref={ref} api={api}{...props} receiveShadow>
       <boxBufferGeometry args={[10,0,10]}/>
       <meshPhysicalMaterial color={'green'}/>
     </mesh>
   )
 }
-
 export default Ground
